d286cfe51264db118af5a43d9f56599a86d667d6
Peter <peter@Peters-MBP-3.(none)>
2025-04-19 17:36:51 -0400

    fixed build process

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
+// frontend/vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: "/time-o-grapher/",
   build: {
     outDir: "../docs",
+    emptyOutDir: true,
   },
 });
